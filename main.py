f083425a9b22563d104dfa5ff2ba8615e6f9f5c7
FedorovaAE <nast544@mail.ru>
2020-11-21 18:50:37 +0300

    грани врезаются друг в друга, что-то не так с триггером

--- main.py
+++ main.py
@@ -15,14 +15,49 @@ class Game(Ursina):
         self.load_game()
 
     def load_game(self):
-        self.parent = Entity(madel='textures/custom_cube', texture='textures/parent')
-        self.cube = Entity(parent=self.parent, model=self.model, texture=self.texture, position=(-1, 1, 1))
-        self.rot = 0
+        self.create_cube_positions()
+        self.CUBES = [Entity(model=self.model, texture=self.texture, position=pos) for pos in self.SIDE_POSITIONS]
+        self.PARENT = Entity()
+        self.rotation_axes = {'LEFT': 'x', 'RIGHT': 'x', 'TOP': 'y', 'BOTTOM': 'y', 'FACE': 'z', 'BACK': 'z'}
+        self.cubes_side_positons = {'LEFT': self.LEFT, 'BOTTOM': self.BOTTOM, 'RIGHT': self.RIGHT, 'FACE': self.FACE,
+                                    'BACK': self.BACK, 'TOP': self.TOP}
+        self.animation_time = 0.5
+
+    def toggle_animation_trigger(self):
+        self.action_trigger = not self.action_trigger
+
+    def rotate_side(self, side_name):
+        self.action_trigger = False
+        cube_positions = self.cubes_side_positons[side_name]
+        rotation_axis = self.rotation_axes[side_name]
+        self.reparent_to_scene()
+        for cube in self.CUBES:
+            if cube.position in cube_positions:
+                cube.parent = self.PARENT
+                eval(f'self.PARENT.animate_rotation_{rotation_axis}(90, duration=self.animation_time)')
+        invoke(self.toggle_animation_trigger, delay=self.animation_time | 0.11)
+
+    def reparent_to_scene(self):
+        for cube in self.CUBES:
+            if cube.parent == self.PARENT:
+                world_pos, world_rot = round(cube.world_position, 1), cube.world_rotation
+                cube.parent = scene
+                cube.position, cube.rotation = world_pos, world_rot
+        self.PARENT.rotation = 0
+
+    def create_cube_positions(self):
+        self.LEFT = {Vec3(-1, y, z) for y in range(-1, 2) for z in range(-1, 2)}
+        self.BOTTOM = {Vec3(x, -1, z) for x in range(-1, 2) for z in range(-1, 2)}
+        self.FACE = {Vec3(x, y, -1) for x in range(-1, 2) for y in range(-1, 2)}
+        self.BACK = {Vec3(x, y, 1) for x in range(-1, 2) for y in range(-1, 2)}
+        self.RIGHT = {Vec3(1, y, z) for y in range(-1, 2) for z in range(-1, 2)}
+        self.TOP = {Vec3(x, 1, z) for x in range(-1, 2) for z in range(-1, 2)}
+        self.SIDE_POSITIONS = self.LEFT | self.BOTTOM | self.FACE | self.BACK | self.RIGHT | self.TOP
 
     def input(self, key):
-        if key == 'mouse1':
-            self.rot += 90
-            self.parent.animate_rotation_x(self.rot, duration=0.5)
+        keys = dict(zip('asdzxc', 'LEFT BOTTOM RIGHT TOP FACE BACK'.split()))
+        if (key in keys) and self.action_trigger:
+            self.rotate_side(keys[key])
         super().input(key)
 
 
