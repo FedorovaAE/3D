e7ba718a2e304b7834f7d3cbadfa0ab0d07688ea
FedorovaAE <nast544@mail.ru>
2020-11-29 00:25:36 +0300

    кубик, меню, пришелец не выглядит как пришелец но я старалась

--- aliens_game.py
+++ aliens_game.py
@@ -1,9 +1,8 @@
-from ursina import *
 from ursina import camera
 from game_obj import *
 
 
-class GameAliens(Ursina):
+class Game(Ursina):
     def __init__(self):
         super().__init__()
         window.color = color.black
@@ -15,15 +14,73 @@ class GameAliens(Ursina):
         camera.rotation_x = -57
 
     def create_map(self, map_size):
-        Entity(model='quad', scale=map_size, position=(map_size // 2, map_size // 2, 0), color=color.violet)
-        Entity(model=Grid(map_size, map_size), scale=map_size, position=(map_size // 2, map_size // 2, -0.01),
-               color=color.white)
+        Entity(model='quad', scale=map_size, position=(map_size // 2, map_size // 2, 0), color=color.dark_gray)
+        Entity(model=Grid(map_size, map_size), scale=map_size,
+               position=(map_size // 2, map_size // 2, -0.01), color=color.white)
 
     def new_game(self):
         scene.clear()
         self.create_map(self.MAP_SIZE)
-        self.fruit = Fruit(self.MAP_SIZE, model='sphere', color=color.red)
+        self.apple = Fruit(self.MAP_SIZE, model='sphere', color=color.red)
+        self.player = Player(self.MAP_SIZE)
+        self.villain = Villain(self.MAP_SIZE)
+        self.num_villain = [self.villain]
+        self.player_boll = Boll(self.player.speed, self.player.direction, self.player.segment_positions[-1])
+
+    def input(self, key):
+        if key == '2':
+            camera.rotation_x = 0
+            camera.position = (self.MAP_SIZE // 2, self.MAP_SIZE // 2, -50)
+        elif key == '3':
+            camera.position = (self.MAP_SIZE // 2, -20.5, -20)
+            camera.rotation_x = -57
+        elif key == '1':
+            if self.player.direction != Vec3(0, 0, 0):
+                self.player_boll = Boll(self.player.speed, self.player.direction, self.player.segment_positions[-1],
+                                    model='sphere', color=color.orange)
+        super().input(key)
+
+    def kill_villain(self):
+        for i in range(len(self.num_villain)):
+            if self.player_boll.position == self.num_villain[i].segment_positions[-1]:
+                del self.num_villain[i]
+
+    def check_fruit_eaten(self):
+        if self.player.segment_positions[-1] == self.apple.position:
+            self.player.add_segment()
+            self.apple.new_position()
+            self.create_villain()
+
+    def create_villain(self):
+        self.num_villain.append(Villain(self.MAP_SIZE))
+
+    def check_game_over(self):
+        player = self.player.segment_positions
+        still_run = True
+        for i in range(len(self.num_villain)):
+            if self.player.segment_positions[-1] == self.num_villain[i].segment_positions[-1]:
+                still_run = False
+        if (0 < player[-1][0] < self.MAP_SIZE) and (0 < player[-1][1] < self.MAP_SIZE) and still_run:
+            return
+        print_on_screen('GAME OVER', position=(-0.7, 0.1), scale=10, duration=1)
+        self.player.direction = Vec3(0, 0, 0)
+        self.player.permissions = dict.fromkeys(self.player.permissions, 0)
+        invoke(self.new_game, delay=1)
+
+    def update(self):
+        print_on_screen(f'Score: {self.player.score}', position=(-0.75, 0.45), scale=3, duration=1 / 20)
+        print_on_screen(f': {self.villain.segment_positions[-1]}', position=(0.15, 0.45), scale=3, duration=1 / 20)
+        self.kill_villain()
+        self.check_fruit_eaten()
+        self.check_game_over()
+        self.player.run()
+        self.player_boll.run()
+        for i in range(len(self.num_villain)):
+            self.num_villain[i].run()
+        self.player_boll.run()
 
 
-game = GameAliens()
-game.run()
+if __name__ == '__main__':
+    game = Game()
+    update = game.update
+    game.run()
--- game_obj.py
+++ game_obj.py
@@ -3,10 +3,120 @@ from random import randrange
 
 
 class Fruit(Entity):
-    def __init__(self, map_size, **kwargs):
+    def __init__(self, MAP_SIZE, **kwargs):
         super().__init__(**kwargs)
-        self.MAP_SIZE = map_size
+        self.MAP_SIZE = MAP_SIZE
         self.new_position()
 
     def new_position(self):
         self.position = (randrange(self.MAP_SIZE) + 0.5, randrange(self.MAP_SIZE) + 0.5, -0.5)
+
+
+class Player:
+    def __init__(self, map_size):
+        self.MAP_SIZE = map_size
+        self.segment_length = 1
+        self.position_length = self.segment_length + 1
+        self.segment_positions = [Vec3(randrange(map_size) + 0.5, randrange(map_size) + 0.5, -0.5)]
+        self.segment_entities = []
+        self.create_segment(self.segment_positions[0])
+        self.directions = {'a': Vec3(-1, 0, 0), 'd': Vec3(1, 0, 0), 'w': Vec3(0, 1, 0), 's': Vec3(0, -1, 0)}
+        self.direction = Vec3(0, 0, 0)
+        self.permissions = {'a': 1, 'd': 1, 'w': 1, 's': 1}
+        self.taboo_movement = {'a': 'd', 'd': 'a', 'w': 's', 's': 'w'}
+        self.speed, self.score = 12, 0
+        self.frame_counter = 0
+
+    def add_segment(self):
+        self.score += 1
+        self.speed = max(self.speed - 1, 5)
+
+    def create_segment(self, position):
+        entity = Entity(position=position)
+        Entity(model='textures/aliens', color=color.green, position=position).add_script(
+            SmoothFollow(speed=12, target=entity, offset=(0, 0, 0)))
+        self.segment_entities.insert(0, entity)
+
+    def run(self):
+        self.frame_counter += 1
+        if not self.frame_counter % self.speed:
+            self.control()
+            self.segment_positions.append(self.segment_positions[-1] + self.direction)
+            self.segment_positions = self.segment_positions[-self.segment_length:]
+            for segment, segment_position in zip(self.segment_entities, self.segment_positions):
+                segment.position = segment_position
+
+    def control(self):
+        for key in 'wasd':
+            if held_keys[key] and self.permissions[key]:
+                self.direction = self.directions[key]
+                self.permissions = dict.fromkeys(self.permissions, 1)
+                self.permissions[self.taboo_movement[key]] = 0
+                break
+
+
+class Villain:
+    def __init__(self, map_size):
+        self.MAP_SIZE = map_size
+        self.segment_length = 1
+        self.position_length = self.segment_length + 1
+        self.segment_positions = [Vec3(randrange(map_size) + 0.5, randrange(map_size) + 0.5, -0.5)]
+        self.segment_entities = []
+        self.create_segment(self.segment_positions[0])
+        self.directions = {'r': Vec3(-1, 0, 0), 't': Vec3(1, 0, 0), 'y': Vec3(0, 1, 0), 'u': Vec3(0, -1, 0)}
+        self.direction = Vec3(1, 0, 0)
+        self.permissions = {'r': 1, 't': 1, 'y': 1, 'u': 1}
+        self.taboo_movement = {'r': 't', 't': 'r', 'y': 'u', 'u': 'y'}
+        self.speed, self.score = 12, 0
+        self.frame_counter = 0
+
+    def add_segment(self):
+        self.score += 1
+        self.speed = max(self.speed - 1, 5)
+
+    def create_segment(self, position):
+        entity = Entity(position=position)
+        Entity(model='sphere', color=color.blue, position=position).add_script(
+            SmoothFollow(speed=12, target=entity, offset=(0, 0, 0)))
+        self.segment_entities.insert(0, entity)
+
+    def run(self):
+        self.frame_counter += 1
+        if not self.frame_counter % self.speed:
+            self.control()
+            snake = self.segment_positions
+            if 1 > snake[-1][0]:
+                self.direction = self.directions['t']
+            elif self.MAP_SIZE - 1 < snake[-1][0]:
+                self.direction = self.directions['r']
+            elif 1 > snake[-1][1]:
+                self.direction = self.directions['y']
+            elif self.MAP_SIZE - 1 < snake[-1][1]:
+                self.direction = self.directions['u']
+            if 0 < snake[-1][0] < self.MAP_SIZE and 0 < snake[-1][1] < self.MAP_SIZE:
+                self.segment_positions.append(self.segment_positions[-1] + self.direction)
+                self.segment_positions = self.segment_positions[-self.segment_length:]
+                for segment, segment_position in zip(self.segment_entities, self.segment_positions):
+                    segment.position = segment_position
+
+    def control(self):
+        dict_vi = {1: 'r', 2: 't', 3: 'y', 4: 'u'}
+        key = dict_vi[random.randint(1, 4)]
+        if self.permissions[key]:
+            self.direction = self.directions[key]
+            self.permissions = dict.fromkeys(self.permissions, 1)
+            self.permissions[self.taboo_movement[key]] = 0
+
+
+class Boll(Entity):
+    def __init__(self, speed, direction, position, **kwargs):
+        super().__init__(**kwargs)
+        self.speed = speed
+        self.direction = direction
+        self.frame_counter = 0
+        self.position = position
+
+    def run(self):
+        self.speed -= 20
+        if not self.frame_counter % self.speed:
+            self.position += self.direction
